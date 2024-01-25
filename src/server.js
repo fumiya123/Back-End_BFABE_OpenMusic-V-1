e763b8c993e44f0eef7f1173650e4d0a4ec16c01
Habib <habibalhaq53043@gmail.com>
2024-01-25 21:00:37 +0700

    start OpenMusic-V-1

---
+++ src/server.js
@@ -0,0 +1,53 @@
+// mengimpor dotenv dan menjalankan konfigurasinya
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const songs = require('./api/musics');
+const SongsService = require('./services/postgres/SongsService');
+const SongsValidator = require('./validator/musics');
+const ClientError = require('./exceptions/ClientError');
+
+const init = async () => {
+  const songsService = new SongsService();
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+
+    const { response } = request;
+
+    if (response instanceof ClientError) {
+      // membuat response baru dari response toolkit sesuai kebutuhan error handling
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+
+      newResponse.code(response.statusCode);
+
+      return newResponse;
+    }
+    // jika bukan ClientError, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+    return response.continue || response;
+  });
+  
+  await server.register({
+    plugin: songs,
+    options: {
+      service: songsService,
+      validator: SongsValidator,
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
